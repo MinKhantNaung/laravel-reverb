93ce67cd058673c75eb05932593b6f47992415b3
Minkhantnaung@securelink <minkhantnaungkzw@gmail.com>
2024-12-29 21:47:15 +0630

    Add: listening to event in dashboard with alpine x-init

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,8 +8,17 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                <div
+                    x-init="
+                        Echo.channel('chat')
+                            {{-- Note: in listen method, type your Laravel event name --}}
+                            .listen('ExampleEvent', (event) => {
+                                console.log(event)
+                            })
+                    "
+                    class="p-6 text-gray-900"
+                >
+                    You are logged in.
                 </div>
             </div>
         </div>
