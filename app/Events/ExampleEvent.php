b88effef9c2d0c7921c9e5b8ac1c65ed3ba314f3
Minkhantnaung@securelink <minkhantnaungkzw@gmail.com>
2024-12-29 21:56:16 +0630

    Update: adding data to events

--- app/Events/ExampleEvent.php
+++ app/Events/ExampleEvent.php
@@ -2,6 +2,7 @@
 
 namespace App\Events;
 
+use App\Models\User;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
@@ -15,10 +16,7 @@ class ExampleEvent implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
-    /**
-     * Create a new event instance.
-     */
-    public function __construct()
+    public function __construct(public User $user)
     {
         //
     }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Events\ExampleEvent;
 use App\Http\Controllers\ProfileController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,7 +10,7 @@ Route::get('/', function () {
 });
 
 Route::get('/broadcast', function () {
-    broadcast(new ExampleEvent());
+    broadcast(new ExampleEvent(User::find(1)));
 });
 
 Route::get('/dashboard', function () {
