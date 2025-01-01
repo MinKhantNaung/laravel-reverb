0e1553d4039334e3e4ccf939ea961595d9af72e6
Minkhantnaung@securelink <minkhantnaungkzw@gmail.com>
2025-01-01 17:40:52 +0630

    Presence channel: Detecting who's in here

--- resources/views/room.blade.php
+++ resources/views/room.blade.php
@@ -9,11 +9,23 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div
+                    x-data="{
+                        usersHere: []
+                    }"
                     x-init="
                         Echo.join('rooms.{{ $room->id }}')
+                            .here((users) => {
+                                usersHere = users
+                            })
                     "
                     class="p-6 text-gray-900"
                 >
+                    <div>
+                        <h2 class="font-semibold text-lg">Users Here</h2>
+                        <template x-for="user in usersHere">
+                            <div x-text="user.name"></div>
+                        </template>
+                    </div>
                 </div>
             </div>
         </div>
