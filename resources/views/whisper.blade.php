768afa0a536fdf69296816b91a2277c3b378302e
Minkhantnaung@securelink <minkhantnaungkzw@gmail.com>
2025-01-02 23:39:48 +0630

    Whispering: everytime a user mouse move, broadcast and send data

--- resources/views/whisper.blade.php
+++ resources/views/whisper.blade.php
@@ -8,7 +8,25 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div x-init="const channel = Echo.private('app')" class="p-6 text-gray-900">
+                <div
+                    x-init="
+                        const channel = Echo.private('app')
+
+                        let width = window.innerWidth
+                        let height = window.innerHeight
+
+                        onmousemove = (e) => {
+                            // in 'mousemove', you can give any names
+                            channel.whisper('mousemove', {
+                                user: {{ json_encode(auth()->user()->only('id', 'name')) }},
+                                position: {
+                                    x: e.x,
+                                    y: e.y
+                                }
+                            })
+                        }
+                    "
+                    class="p-6 text-gray-900">
                     <div class="flex items-center absolute leading-none h-3 space-x-1">
                         <svg fill="none" preserveAspectRatio="none" viewBox="5 5 14 14" class="size-3">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
